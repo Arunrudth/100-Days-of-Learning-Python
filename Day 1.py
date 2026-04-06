554f006d73c46c99645bf94e824288cd1ddbe0bb
Arunrudth <akarunkumar348@gmail.com>
2026-04-06 19:26:10 +0530

    Day 1.py

---
+++ Day 1.py
@@ -0,0 +1,16 @@
+#Today we are gonna learn about the print function, len function, input functions in Python
+#print function
+#print("Welcome to this Journey")
+#Variables
+#name = "Arun"
+#input functions
+#name = input("Please mention your name")
+#len function
+#length = len(name)
+#print(f"Welcome {name}, Your lucky number is {length}")
+# Mini project 1 - Youtube channel name Generator
+print("Welcome to Youtube channel name Generator!!!")
+name = input("Please provide your name")
+niche = input("Please tell me your Niche")
+print(f"Your Channel name suggestion is {name} {niche}")
+print("Thanks for using!!!")
